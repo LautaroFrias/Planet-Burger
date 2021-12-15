11f13d26050e0b51cbc60c56b68b5c5767083663
lautaro.frias <lauta.lp91@gmail.com>
2021-12-15 18:23:27 -0300

    Co-authored-by: Alejo Tomás Clifton Goldney <Monderks@users.noreply.github.com>

--- src/components/ItemCartContainer/ItemCartContainer.js
+++ src/components/ItemCartContainer/ItemCartContainer.js
@@ -3,7 +3,7 @@ import React from "react";
 const ItemCartContainer = () => {
   return (
     <div>
-      <h1>Acá estan todos los productos que se seleccionen</h1>
+      <h1>Acá van todos los productos que se seleccionen</h1>
     </div>
   );
 };
